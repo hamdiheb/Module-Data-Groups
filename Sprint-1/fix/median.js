9e4679432a5540bedcc442a5311e4caf53bddace
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-01-09 11:15:19 +0100

    Fixing Median exercice

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,24 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // const middleIndex = Math.floor(list.length / 2);
+  // let median = 0;
+  // if(list.length % 2 > 0) {
+  //    median = list.splice(middleIndex, 1)[0];
+  // }
+  // else {
+  //    median = list.splice(middleIndex-1, 1)[0]+0.5;
+  // }
+  // return median;
+  let sum=0;
+  for (let i=0;i<list.length;i++){
+    sum+=list[i];
+  }
+
+  const middleIndex=sum/list.length;
+  return middleIndex;
 }
+
+console.log(calculateMedian([1, 2, 3, 4]))
 
 module.exports = calculateMedian;
