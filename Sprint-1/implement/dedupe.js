a89b3dc0c743fcf36a6e923abbfbbe3b678bfeb3
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-01-11 19:56:54 +0100

    Dedupe exercice

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,6 +1,18 @@
 function dedupe(list) {
-   
+
+    if(list.length === 0){
+        return list;
+    }
+    else{
+    for(let i=0;i<list.length;i++){
+        for(let j=0;j<list.length;j++){
+            if(list[i] === list[j] && i!=list.length-1  && i!==j){
+                list.splice(j,1);
+            }
+        }
+    }
+}   
+    return list;
 }
 
-
-
+console.log(dedupe(['a','b','c',1]));
