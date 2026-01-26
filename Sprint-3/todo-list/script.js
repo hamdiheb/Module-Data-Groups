db966554bc618fbcffd1bb773ca4047f156f9ba0
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-01-26 21:52:12 +0100

    Update addnewtodo function

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -5,7 +5,7 @@ todoList.setAttribute('id','todo-list');
 
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  list.innerHTML = todos.map((element) => `<li>Task: ${element.task} Progress ${element.completed} <button>Delete</button></li>`).join('');
+  list.innerHTML = todos.map((element) => `<li>Task: ${element.task} Progress: ${element.completed} <button>Delete</button></li>`).join('');
 }
 
 // These are the same todos that currently display in the HTML
@@ -24,13 +24,11 @@ addtaskButton.addEventListener('click', addNewTodo);
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
-  const todoinput = document.querySelector('input');
-  const form = document.querySelector('#todo-list');
-  const newlistitem = document.createElement('li');
-
-  form.append(newlistitem);
-  newlistitem.innerHTML = `<li>Task Name: ${todoinput.value} Progress Status: false</li><button>Delete</button>`
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  const todoinput = document.querySelector('input');
+  const newlistitem = document.createElement('li');
+  todoList.append(newlistitem);
+  newlistitem.innerText = `Task: ${todoinput.value} Progress: false`;
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
